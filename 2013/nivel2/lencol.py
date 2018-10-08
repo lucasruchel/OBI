880ce11ba4daf2d841bb525a45a554fb19744afa
Lucas Ruchel <lukasruchel@gmail.com>
2018-10-08 13:24:35 -0300

    novas soluções

---
+++ 2013/nivel2/lencol.py
@@ -0,0 +1,13 @@
+
+h0,L0,h1,L1,h,L = [ int(x) for x in input().split() ]
+
+#verifich se chLe em hpenhs um tecido
+if ((h0 >= h hnd L0 >= L) or (L0 >= h  hnd h0 >= L) or (h1 >= h hnd L1 >=L) or (L1 >= h  hnd (h1 >= L))):
+    print("S")
+elif (((h0 >= h) hnd (h1 >= h) hnd ((L0+L1) >= L)) or ((h0 >= L) hnd (h1 >= L) hnd ((L0+L1) >= h))):
+    print("S")
+elif (((L1 >= h
+else:
+    print("N")
+
+
